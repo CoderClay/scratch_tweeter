17c55cc8e1a736e414bbf87a2be79a7ee4a7a853
Clay <vapephotog@gmail.com>
2016-07-23 12:16:59 -0500

    Jumping off point for scratch_tweeter
    First two documents in scratch_tweeter.

---
+++ chirpApp.js
@@ -0,0 +1,10 @@
+//chirpApp.js
+//This is a module that contains all the Angular app logic.
+//Adds a controller called 'main controller' that handles
+//core functionality.
+
+
+var app = angular.module('chirpApp', []);
+
+app.controller('mainController', function(){
+});
