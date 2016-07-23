1db81f91ff9818ff85ee6a4fe6e84e2ca2a5d7f0
Clay <vapephotog@gmail.com>
2016-07-23 13:00:27 -0500

    Adding authController to my $scope

--- chirpApp.js
+++ chirpApp.js
@@ -1,33 +1,37 @@
 //chirpApp.js
-//This is a module that contains all the Angular app logic.
-//Adds a controller called 'main controller' that handles
-//core functionality.
 
 var app = angular.module('chirpApp', []);
-
-
-//We are invoking a '$scope' object while constructing our mainController,
-//$scope is the 'M' in 'MVC' i.e. it is the path between the 'View'
-//and the 'Controller' through whih we can pass data and functions
-
 app.controller('mainController', function($scope){
-
-  //Next, attach a 'posts' array to store all the posts that are created
-
   $scope.posts = [];
-
-  //Now, we tack on a 'newPost' to $scope to store meta on the post that's
-  //being created, creating an object with the meta fields below
-
   $scope.newPost = {created_by: '', text: '', created_at: ''};
-
-
-  //Here is a function that will add the contents of 'newPosts' to the 'posts'
-  //array whenever the 'Tweet' button is pressed
 
   $scope.post = function(){
     $scope.newPost.created_at = Date.now();
     $scope.posts.push($scope.newPost);
     $scope.newPost = {created_by: '', text: '', created_at: ''};
+  };
+});
+
+
+//Adding our auth controller to $scope here.
+//This adds 'username' and 'password' functionality to our app
+//Here, both 'login' and 'register' use the same authcontroller for ease
+//and simplicity
+app.controller('authController', function($scope){
+  //Here we are adding an empty user to our $scope
+  $scope.user = {username: '', password: ''};
+  //Here we are adding an 'error' message to pass into our scope
+  $scope.error_message = '';
+
+  $scope.login = function(){
+    //This is all semantic place holder garbage because we ont have a backend yet
+    $scope.error_message = 'login request for ' + $scope.user.username;
+    //Above is what we return if the login auth fails
+  };
+
+  $scope.register = function(){
+    //placeholder until authentication is implemented
+    $scope.error_message = 'registeration request for ' + $scope.user.username;
+    //Above is what we return if the registration auth fails
   };
 });
